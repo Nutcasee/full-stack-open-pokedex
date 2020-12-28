34ee0a80b8ef4b7599cd3837cf5592af3cf8d2e5
nutcasee <nexustivi@gmail.com>
2020-12-29 01:35:49 +0700

    front page can be opened, navigate to a particular Pokemon

---
+++ cypress/integration/forgot_cypress_already.js
@@ -0,0 +1,15 @@
+
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+    
+    cy.contains('ivysaur').click()
+  })
+
+  it('navigate from the main page to ivysaur', function() {
+    // cy.visit('http://localhost:5000/pokemon/')
+    cy.contains('chlorophyll')
+  })
+})
